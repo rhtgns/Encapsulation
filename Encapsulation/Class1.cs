ab3a32fc94c004ffba1c0073b4dbc62df9019431
Rohat Güneş <rohatgns44@gmail.com>
2024-08-08 17:16:53 +1000

    Add project files.

---
+++ Encapsulation/Class1.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Encapsulation
+{
+    public class Kitap
+    {
+        public string Marka { get; set; }
+        public string Model { get; set; }
+        public string Renk { get; set; }
+
+
+        private int kapiSayisi;
+
+        public int KapiSayisi
+        {
+
+            //burda kapı saysını kapsülleme işlemi yapacagız
+            get { return kapiSayisi; }
+            set
+            {
+                //eger kapı sayısı 2 ve ya 4 ise deger atayacagız ama degılse ekrana uyarı cıktısı verecegız
+
+                if (value == 2 || value == 4)
+                {
+
+                    kapiSayisi = value;
+                }
+                else
+                {
+                    Console.WriteLine("Uyarı:Kapı sayisi sdaece 2 ve 4 olabilir.");
+                    kapiSayisi = -1; // burda gecerli olmazsa  kapı syısını bir düşürür                }
+
+                }
+            }
+        }
+
+        // yapıcı fonskiyonumuzu yazalım (constructor)
+
+        public Araba(string marka, string model, string renk, int kapiSayisi)
+        {
+            Marka = marka;
+            Model = model;
+            Renk = renk;
+            KapiSayisi = kapiSayisi;//bu kodda en başından planladıgımız kapsülleme devreye giriyor
+        }
+
+    }
---
+++ Encapsulation/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+public class Program
+{
+    public static void Main()
+    {
+        //burada alınan kapı sayisi ile bir araba nesnesi oluştuıacagız 
+        Araba araba1 =new Araba("Ford","Focus","Sarı",4);
+        Console.WriteLine("Araba 1");
+        Console.WriteLine($"Marka: {araba1.Marka}");
+        Console.WriteLine($"Model: {araba1.Model}");
+        Console.WriteLine($"Renk: {araba1.Renk}");
+        Console.WriteLine($"Kapi Sayısı: {araba1.KapSayisi}");
+
+
+        //burda ise gecerli olmayan kapı sayiis degeri ile işlem yapacagız
+
+        Araba araba2 = new Araba("Audi", "A8", "Siyah", 4);
+        Console.WriteLine("\nAraba 2 :");
+        Console.WriteLine($"Marka: {araba2.Marka}");
+        Console.WriteLine($"Marka: {araba2.Model}");
+        Console.WriteLine($"Marka: {araba2.Renk}");
+        Console.WriteLine($"Marka: {araba2.KapSayisi}");
+    }
+
+}
+
+
